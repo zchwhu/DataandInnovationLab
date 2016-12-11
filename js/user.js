4cb0d509e257152ee0b3e73043aaff05ee9c7c26
zch0301 <291053953@qq.com>
2016-12-11 20:52:10 +0800

    用户上传头像预览及编辑器配置

--- js/user.js
+++ js/user.js
@@ -5,4 +5,35 @@ $(function () {
     $(document).on("change","#userAvatar",function (e) {
         imgPreview($(".user-avatar img"),e);
     })
+
+    var editor = new wangEditor('userEditor');
+
+    editor.config.menus = [
+        'source',
+        'bold',
+        'underline',
+        'italic',
+        'strikethrough',
+        'eraser',
+        'forecolor',
+        'bgcolor',
+        'quote',
+        'fontfamily',
+        'fontsize',
+        'head',
+        'unorderlist',
+        'orderlist',
+        'alignleft',
+        'aligncenter',
+        'alignright',
+        'link',
+        'unlink',
+        'table',
+        'img',
+        'undo',
+        'redo',
+        'fullscreen'
+    ];
+
+    editor.create();
 })
