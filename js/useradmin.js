6cf1768cd029173e2f3b245334244f8a88f99630
zch0301 <291053953@qq.com>
2016-12-13 20:15:05 +0800

    用户后台管理功能

---
+++ js/useradmin.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2016/12/13 0013.
+ */
+$(function () {
+    $(document).on("click",".user-add-btn",function () {
+        $(".user-add-modal").modal();
+    }).on("mouseover",".admin-tooltip",function () {
+        $(this).tooltip("show");
+    }).on("click",".user-bind-btn",function () {
+        $(".user-bind-modal").modal();
+    }).on("click",".user-del-btn",function () {
+        if(confirm("Are you sure you want to delete the user?")){
+            $(this).parents("tr").remove();
+        }
+    });
+
+    $('.user-add-modal').on('hidden.bs.modal', function (e) {
+        $(this).find(":input").val('');
+    })
+
+    $('.user-bind-modal').on('hidden.bs.modal', function (e) {
+        $(this).find("input[type='radio']").prop("checked",false);
+    })
+})
