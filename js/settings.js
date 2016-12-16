e7f5610b35896927bec57344d2b42261354f45fa
zch0301 <291053953@qq.com>
2016-12-16 11:04:47 +0800

    增加了标签编辑功能

--- js/settings.js
+++ js/settings.js
@@ -10,4 +10,59 @@ $(function () {
             })
         }
     })
+
+    $(document).on("click",".tag-edit",function () {
+        var $ele = $(this).parent(".tag"),
+            $name = $ele.find(".tag-name"),
+            $input = $ele.find(".tag-input");
+        if($input.is(":hidden")){
+            $input.find("input").val($name.text())
+            $input.show();
+            $name.hide();
+        }else{
+            $input.hide();
+            $name.show();
+        }
+    })
+
+    $(document).on("click",".tag-confirm",function () {
+        var $ele = $(this).parent(".tag-input"),
+            $input = $ele.find("input"),
+            $parent = $ele.parent(".tag"),
+            $name = $parent.find(".tag-name");
+        if($input.val().length>0){
+            $name.text($input.val()).show();
+            $ele.hide();
+        }else{
+            if(confirm("Are you sure to delete this tag?")){
+                $parent.fadeOut(500,function () {
+                    $parent.remove();
+                })
+            }
+        }
+    })
+
+    $(document).on("keydown",".tag-input input",function (e) {
+        var $input = $(this).parent(".tag-input"),
+            $parent = $input.parent(".tag"),
+            $name = $parent.find(".tag-name");
+        switch(e.keyCode){
+            case 13:{
+                e.preventDefault();
+                $name.text($(this).val()).show();
+                $input.hide();
+                break;
+            }
+            case 8:{
+                if($(this).val().length==0){
+                    if(confirm("Are you sure to delete this tag?")){
+                        $parent.fadeOut(500,function () {
+                            $parent.remove();
+                        })
+                    }
+                }
+                break;
+            }
+        }
+    })
 })
